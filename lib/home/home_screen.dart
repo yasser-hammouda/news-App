3b8debd9d810ef32dec586b9854968f23356df71
Yasser Ahmed <87115659+yasser-hammouda@users.noreply.github.com>
2024-08-08 23:46:04 +0300

    Api Manager Integration Tab Widget

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/app_colors.dart';
+import 'package:news_app/home/tabs/tab_widget.dart';
+import 'package:news_app/model/SourceResponse.dart';
+import 'package:news_app/model/api_manager.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName ='home_screen';
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -19,6 +27,61 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.transparent,
           appBar: AppBar(
             title: Text('News App', style: Theme.of(context).textTheme.titleLarge,),
+          ),
+          body: FutureBuilder<SourceResponse?>(
+              future: ApiManager.getSources(),
+              builder: (context,snapShot) {
+                if(snapShot.connectionState == ConnectionState.waiting){
+                  return Center( child: CircularProgressIndicator(
+                    color: AppColors.primaryLightColor,
+                  ),);
+                }
+                else if (snapShot.hasError){   // error from my side
+                  return Column(
+                    children: [
+                      Text('Something went wrong!!'),
+                      ElevatedButton(
+                          onPressed: (){},
+                          child: Text('Try Again..'))
+                    ],
+                  );
+                }
+                // check if snapShot.data is null
+                if (snapShot.data == null){
+                  return Column(
+                    children: [
+                      Text('No data available!'),
+                      ElevatedButton(
+                          onPressed: (){
+                            ApiManager.getSources();
+                            setState(() {});
+                          },
+                          child: Text('Try Again..'))
+                    ],
+                  );
+                }
+                //server has reply with response => success or fail
+                if(snapShot.data!.status != 'ok'){
+                  return Column(
+                    children: [
+                      Text(snapShot.data!.message! ?? 'Unknown error'),
+                      ElevatedButton(
+                          onPressed: (){
+                            ApiManager.getSources();
+                            setState(() {});
+                          },
+                          child: Text('Try Again..'))
+                    ],
+                  );
+                }
+                // I'm sure I have success response
+                var sourceList =snapShot.data!.sources!;
+                return TabWidget(sourcesList: sourceList,);
+                // return ListView.builder(itemBuilder: (context,index){
+                //   return Text(sourceList[index].name?? " ");
+                // },itemCount: sourceList.length,
+                // );
+              }
           ),
         ),
       ],
